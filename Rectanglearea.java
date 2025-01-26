7f5895932c3fc2ac7a2fee4b623ffcf69a3e5f31
Vangavarsha12 <varshavanga38@gmail.com>
2025-01-26 18:25:37 +0530

    Add files via upload

---
+++ Rectanglearea.java
@@ -0,0 +1,15 @@
+public class Rectanglearea {
+    public static void main(String[] args) {
+        int lengthint=10;
+        int widthint=5;
+        int areaint=lengthint*widthint;
+        System.out.println("Area of the rectangle:"+areaint);
+        
+
+        float lengthfloat=20;
+        float widthfloat=10;
+        float areafloat=lengthfloat*widthfloat;
+        System.out.println("Area of the rectangle:"+areafloat);
+    }
+    
+}
